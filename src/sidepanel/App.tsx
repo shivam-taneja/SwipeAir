917ceeb1f8cd080a2279d9d696aa4ca1734c31de
ShivamTaneja <shiivamtaneja@gmail.com>
2026-03-30 13:02:09 +0530

    feat: cleanup + add tailwind

--- src/sidepanel/App.tsx
+++ src/sidepanel/App.tsx
@@ -2,7 +2,7 @@ import crxLogo from '@/assets/crx.svg'
 import reactLogo from '@/assets/react.svg'
 import viteLogo from '@/assets/vite.svg'
 import HelloWorld from '@/components/HelloWorld'
-import './App.css'
+
 
 export default function App() {
   return (
